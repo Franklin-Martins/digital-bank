b3c888c10e5885d8d4fce29ed8917bdecf6fee32
Franklinxd <gentilezaxd@gmail.com>
2022-04-17 02:30:14 -0400

    Create and finish card module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import config from './config/database_config';
 import { UsersModule } from './users/users.module';
+import { CardsModule } from './cards/cards.module';
 
 @Module({
   imports: [
     UsersModule,
-    TypeOrmModule.forRoot(config(__dirname))
+    TypeOrmModule.forRoot(config(__dirname)),
+    CardsModule
   ],
   controllers: [],
   providers: [],
